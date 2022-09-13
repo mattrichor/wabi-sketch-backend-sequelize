a4adb8418bca31e83c631d7a5fb06a9c2e9122f4
mattrichor <matt.a.geyer@gmail.com>
2022-09-13 18:38:07 -0400

    changing things back

--- config/config.js
+++ config/config.js
@@ -27,8 +27,7 @@ module.exports = {
     dialect: 'postgres'
   },
   production: {
-    database: 'wabi_sketch_production',
-    // use_env_variable: 'DATABASE_URL',
+    use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
     dialectOptions: {
       ssl: {
